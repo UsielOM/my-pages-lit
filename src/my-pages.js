8c0e77f4ab777e303d29d49f5ab04d1898205cec
UsielOM <usiel31197@outlook.es>
2022-09-19 15:25:45 -0500

    Implementando bootstrap

--- src/my-pages.js
+++ src/my-pages.js
@@ -24,15 +24,20 @@ export class MyPages extends LitElement {
         return html `<article> ${this.article.text}</article>`;
     }
     footerTemplate() {
-        return html `<footer>Your footer here.</footer>`;
+        return html `<footer>Your footer here.</footer>
+        
+        `;
     }
 
     render() {
         return html `
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         ${this.headerTemplate()}
         ${this.articleTemplate()}
         ${this.footerTemplate()}
-        
+        <button class="btn btn-danger">hola</button>        
+       
+
         `;
     }
 }
